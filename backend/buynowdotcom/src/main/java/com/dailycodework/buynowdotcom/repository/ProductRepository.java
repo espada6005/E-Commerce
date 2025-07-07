e06bff661f18f177537fd693d88a80c2f197080b
espada <yua2479@gmail.com>
2025-07-07 23:04:15 +0900

    add @Query to findByName method & add existsByNameAndBrand method

--- backend/buynowdotcom/src/main/java/com/dailycodework/buynowdotcom/repository/ProductRepository.java
+++ backend/buynowdotcom/src/main/java/com/dailycodework/buynowdotcom/repository/ProductRepository.java
@@ -3,6 +3,7 @@ package com.dailycodework.buynowdotcom.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.dailycodework.buynowdotcom.model.Product;
 
@@ -14,8 +15,11 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
     List<Product> findByNameAndBrand(String brand, String name);
 
+    @Query("SELECT p FROM Product p WHERE LOWER(p.name) LIKE LOWER(CONCAT('%', :name, '%'))")
     List<Product> findByName(String name);
 
     List<Product> findByBrand(String brand);
+
+    boolean existsByNameAndBrand(String name, String brand);
 
 }
