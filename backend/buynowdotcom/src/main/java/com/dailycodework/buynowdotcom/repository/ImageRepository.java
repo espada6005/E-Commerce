b02990872ae6411d7ba79f2d8417771436050a9c
espada <yua2479@gmail.com>
2025-08-05 20:58:08 +0900

    add ImageRepository interface

---
+++ backend/buynowdotcom/src/main/java/com/dailycodework/buynowdotcom/repository/ImageRepository.java
@@ -0,0 +1,9 @@
+package com.dailycodework.buynowdotcom.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dailycodework.buynowdotcom.model.Image;
+
+public interface ImageRepository extends JpaRepository<Image, Long> {
+
+}
