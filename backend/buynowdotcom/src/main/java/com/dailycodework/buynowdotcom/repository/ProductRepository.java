41043c1e810fe9cb44d501d40b5944f869fe06de
espada <yua2479@gmail.com>
2025-08-07 22:15:03 +0900

    add findAllByCategoryId method

--- backend/buynowdotcom/src/main/java/com/dailycodework/buynowdotcom/repository/ProductRepository.java
+++ backend/buynowdotcom/src/main/java/com/dailycodework/buynowdotcom/repository/ProductRepository.java
@@ -22,4 +22,6 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
     boolean existsByNameAndBrand(String name, String brand);
 
+    List<Product> findAllByCategoryId(Long categoryId);
+
 }
