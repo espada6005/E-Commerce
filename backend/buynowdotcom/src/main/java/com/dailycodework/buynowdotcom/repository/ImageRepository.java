8efc18087e25fd6d898f3cbfb6b9b04bb4ceb971
espada <yua2479@gmail.com>
2025-08-07 22:06:49 +0900

    add findAllByCategoryId method

--- backend/buynowdotcom/src/main/java/com/dailycodework/buynowdotcom/repository/ImageRepository.java
+++ backend/buynowdotcom/src/main/java/com/dailycodework/buynowdotcom/repository/ImageRepository.java
@@ -1,9 +1,13 @@
 package com.dailycodework.buynowdotcom.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.dailycodework.buynowdotcom.model.Image;
 
 public interface ImageRepository extends JpaRepository<Image, Long> {
+
+    List<Image> findByProductId(Long id);
 
 }
