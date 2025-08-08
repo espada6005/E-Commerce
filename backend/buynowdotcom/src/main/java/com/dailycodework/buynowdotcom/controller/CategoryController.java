f3edeb47e53f7b6f0b77de7b1bb1684a5a7309ea
espada <yua2479@gmail.com>
2025-08-08 21:40:15 +0900

    add controllers

---
+++ backend/buynowdotcom/src/main/java/com/dailycodework/buynowdotcom/controller/CategoryController.java
@@ -0,0 +1,64 @@
+package com.dailycodework.buynowdotcom.controller;
+
+import java.util.List;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dailycodework.buynowdotcom.model.Category;
+import com.dailycodework.buynowdotcom.response.ApiResponse;
+import com.dailycodework.buynowdotcom.service.category.CategoryService;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("${api.prefix}/categories")
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    @GetMapping("/all")
+    public ResponseEntity<ApiResponse> getAllCategories() {
+        List<Category> categories = categoryService.getAllCategories();
+        return ResponseEntity.ok(new ApiResponse("Found", categories));
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<ApiResponse> addCategory(@RequestBody Category category) {
+            Category theCategory = categoryService.addCategory(category);
+            return ResponseEntity.ok(new ApiResponse("Success", theCategory));
+    }
+
+    @GetMapping("/category/{id}/category")
+    public ResponseEntity<ApiResponse> getCategoryById(@RequestParam Long id) {
+            Category theCategory = categoryService.findCategoryById(id);
+            return ResponseEntity.ok(new ApiResponse("Success", theCategory));
+    }
+
+    @GetMapping("/category/{name}/category")
+    public ResponseEntity<ApiResponse> getCategoryByName(@PathVariable String name) {
+            Category theCategory = categoryService.findCategoryByName(name);
+            return ResponseEntity.ok(new ApiResponse("Found", theCategory));
+    }
+
+    @PutMapping("/category/{id}/update")
+    public ResponseEntity<ApiResponse> updateCategory(@PathVariable Long id, @RequestBody Category category) {
+        Category updatedCategory = categoryService.updateCategory(category, id);
+        return ResponseEntity.ok(new ApiResponse("Update success!", updatedCategory));
+    }
+
+    @DeleteMapping("/category/{id}/delete")
+    public ResponseEntity<ApiResponse> deleteCategory(@PathVariable Long id) {
+        categoryService.deleteCategory(id);
+        return ResponseEntity.ok(new ApiResponse("Found", null));
+    }
+
+}
---
+++ backend/buynowdotcom/src/main/java/com/dailycodework/buynowdotcom/controller/ProductController.java
@@ -0,0 +1,105 @@
+package com.dailycodework.buynowdotcom.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dailycodework.buynowdotcom.dtos.ProductDto;
+import com.dailycodework.buynowdotcom.model.Product;
+import com.dailycodework.buynowdotcom.request.AddProductRequest;
+import com.dailycodework.buynowdotcom.request.ProductUpdateRequest;
+import com.dailycodework.buynowdotcom.response.ApiResponse;
+import com.dailycodework.buynowdotcom.service.product.ProductService;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("${api.prefix}/products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    @GetMapping("/all")
+    public ResponseEntity<ApiResponse> getAllProducts() {
+        List<Product> products = productService.getAllProducts();
+        List<ProductDto> convertdProducts = productService.getConvertedProducts(products);
+        return ResponseEntity.ok(new ApiResponse("Found", convertdProducts));
+    }
+
+    @GetMapping("/product/{productId}/product")
+    public ResponseEntity<ApiResponse> getProductById(@PathVariable Long productId) {
+        Product product = productService.getProductById(productId);
+        ProductDto productDto = productService.convertToDto(product);
+        return ResponseEntity.ok(new ApiResponse("Found!", productDto));
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<ApiResponse> addPProduct(@RequestBody AddProductRequest product) {
+        Product theProduct = productService.addProduct(product);
+        ProductDto productDto = productService.convertToDto(theProduct);
+        return ResponseEntity.ok(new ApiResponse("add product success!", productDto));
+    }
+
+    @PutMapping("/product/{productId}/update")
+    public ResponseEntity<ApiResponse> updateProduct(@PathVariable Long productId, @RequestBody ProductUpdateRequest productRequest) {
+        Product theProduct = productService.updateProduct(productRequest, productId);
+        ProductDto productDto = productService.convertToDto(theProduct);
+        return ResponseEntity.ok(new ApiResponse("Product updated success!", productDto));
+    }
+
+    @DeleteMapping("/product/{productId}/delete")
+    public ResponseEntity<ApiResponse> deleteProduct(@PathVariable Long productId) {
+        productService.deleteProductById(productId);
+        return ResponseEntity.ok(new ApiResponse("Delete product success!", productId));
+    }
+
+    @GetMapping("/products/by/brand-and-name")
+    public ResponseEntity<ApiResponse> getProductByBrandAndName(@RequestParam String brandName, @RequestParam String productName) {
+        List<Product> products = productService.getProductsByBrandAndName(brandName, productName);
+        List<ProductDto> convertedProducts = productService.getConvertedProducts(products);
+        return ResponseEntity.ok(new ApiResponse("success", convertedProducts));
+    }
+
+    @GetMapping("/products/by/category-and-brand")
+    public ResponseEntity<ApiResponse> getProductByCategoryAndBrand(@RequestParam String category, @RequestParam String brandName) {
+        List<Product> products = productService.getProductsByCategoryAndBrand(category, brandName);
+        List<ProductDto> convertedProducts = productService.getConvertedProducts(products);
+        return ResponseEntity.ok(new ApiResponse("success", convertedProducts));
+    }
+
+    @GetMapping("/products/{name}/products")
+    public ResponseEntity<ApiResponse> getProductByName(@PathVariable String param) {
+        List<Product> products = productService.getProductsByName(param);
+        List<ProductDto> convertedProducts = productService.getConvertedProducts(products);
+        return ResponseEntity.ok(new ApiResponse("success", convertedProducts));
+    }
+    
+    @GetMapping("/product/by-brand")
+    public ResponseEntity<ApiResponse> findProductByBrand(@RequestParam String brand) {
+        List<Product> products = productService.getProductsByBrand(brand);
+        List<ProductDto> convertedProducts = productService.getConvertedProducts(products);
+        return ResponseEntity.ok(new ApiResponse("success", convertedProducts));
+    }
+
+    @GetMapping("/product/{category}/all/products")
+    public ResponseEntity<ApiResponse> findProductByCategory(@PathVariable String category) {
+        List<Product> products = productService.getProductsByCategory(category);
+        List<ProductDto> convertedProducts = productService.getConvertedProducts(products);
+        return ResponseEntity.ok(new ApiResponse("success", convertedProducts));
+    }
+    
+
+}
