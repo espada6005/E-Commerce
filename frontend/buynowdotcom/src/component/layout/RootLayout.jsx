48fd404f86532104bb0b4027eed674ad3498ce87
espada <yua2479@gmail.com>
2025-08-14 21:40:29 +0900

    add root layout component

---
+++ frontend/buynowdotcom/src/component/layout/RootLayout.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Outlet } from "react-router-dom"
+import NavBar from './NavBar'
+
+const RootLayout = () => {
+    return (
+        <main>
+            <NavBar />
+            <div>
+                <Outlet />
+            </div>
+        </main>
+    )
+}
+
+export default RootLayout
