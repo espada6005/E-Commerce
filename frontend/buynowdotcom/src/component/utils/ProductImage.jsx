82c19133bf627e2d558356e207d029960c7d7b48
espada <yua2479@gmail.com>
2025-08-19 21:25:49 +0900

    add ProductImage component

---
+++ frontend/buynowdotcom/src/component/utils/ProductImage.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react'
+
+export const ProductImage = ({ productId }) => {
+    const [productImg, setProductImg] = useState(null);
+
+    useEffect(() => {
+        const fetchProductImage = async (id) => {
+            try {
+                const response = await fetch(
+                    `http://localhost:8080/api/v1/images/image${id}`
+                );
+                const bolb = await response.blob();
+                const reader = new FileReader();
+                reader.onloadend = () => {
+                    setProductImg(reader.result);
+                };
+                reader.readAsDataURL(bolb);
+            } catch (error) {
+                console.error("Error fetching image:", error);
+            }
+        };
+
+        if (productId) {
+            fetchProductImage(productId);
+        }
+    }, [productId]);
+
+    if (!productImg) {
+        return null;
+    }
+
+
+    return (
+        <div>
+            <img src={productImg} alt="Product Image" />
+        </div>
+    )
+}
