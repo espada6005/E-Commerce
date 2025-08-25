f58bb1b05e7a64959dd6bb598061c97805484a7e
espada <yua2479@gmail.com>
2025-08-25 21:56:32 +0900

    add getDistinctProductsByName function

---
+++ frontend/buynowdotcom/src/component/servies/ProductService.js
@@ -0,0 +1,10 @@
+import { api } from './api'
+
+export const getDistinctProductsByName = async () => {
+    try {
+        const response = await api.get("/products/distinct/products")
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+}
