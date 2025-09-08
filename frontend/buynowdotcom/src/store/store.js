2a4b4ff4bd5403ee284f15d8f0fcf684727f9b49
espada <yua2479@gmail.com>
2025-09-08 22:39:55 +0900

    add product reducer

--- frontend/buynowdotcom/src/store/store.js
+++ frontend/buynowdotcom/src/store/store.js
@@ -1,10 +1,12 @@
-import { configureStore } from "@reduxjs/toolkit"
-import searchReducer from "./features/searchSlice"
+import { configureStore } from "@reduxjs/toolkit";
+import searchReducer from "./features/searchSlice";
 import categoryReducer from './features/categorySlice';
+import productReducer from './features/productSlice';
 
 export const store = configureStore({
     reducer: {
         search : searchReducer,
         category: categoryReducer,
+        product: productReducer,
     }
 })
